84b1b3cab5c2e14728ebe200ca22132d99f3c1d2
Farid <faridespinoza05@hotmail.com>
2024-03-16 18:22:30 -0500

    every error of the desktop is fixed and finally head style css by DOM work correctly

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-
 // Select the elements
 let second = document.querySelector('.second');
 
-let resultP;
-let copyBtn;
+let resultPClass;
+let copyBtnClass;
+
+let resultPId;
+let copyBtnId;
 
 
 function setAfterBTN(){
-    resultP = document.querySelector('.result-p');
-    copyBtn = document.querySelector('.copy-btn');
+    resultPId = document.getElementById('result-p');
+    copyBtnId = document.getElementById('copy-btn');
+
+    copyBtnClass = document.getElementsByClassName('copy-btn')[0];
+    resultPClass = document.getElementsByClassName('result-p')[0];
     // Set the styles
-    resultP.style.fontSize = '1.2em';
-    resultP.style.color = 'var(--gray-400)';
-    resultP.style.padding = '1.2%';
+    resultPId.style.fontSize = '1.2em';
+    resultPId.style.color = 'var(--gray-400)';
+    resultPId.style.padding = '1.2%';
 
     second.style.display = 'flex';
     second.style.flexDirection = 'column';
@@ -20,16 +25,19 @@ function setAfterBTN(){
     second.style.boxSizing = 'border-box';
     second.style.padding = '2%';
 
-    copyBtn.style.fontSize = '1.5em';
-    copyBtn.style.color = 'var(--dark-blue-300)';
-    copyBtn.style.width = '100%';
-    copyBtn.style.height = '2.5em';
-    copyBtn.style.backgroundColor = 'var(--light-blue-100)';
-    copyBtn.style.border = '1px solid var(--dark-blue-300)';
-    copyBtn.style.borderRadius = '1em';
+    copyBtnId.style.fontSize = '1.5em';
+    copyBtnId.style.color = 'var(--dark-blue-300)';
+    copyBtnId.style.width = '100%';
+    copyBtnId.style.height = '2.5em';
+    copyBtnId.style.border = '1px solid var(--dark-blue-300)';
+    copyBtnId.style.borderRadius = '1em';
+    copyBtnId.onclick = copyText;
+    copyBtnClass.style.transition = '0.5s';
+    copyBtnClass:hover = 'background-color: var(--black-blue-200)';
+
 }
 
-function generateElements(elementType, textContent, className) {
+function generateElements(elementType, textContent, className, a) {
     let newElement = document.createElement(elementType);
     let textNode = document.createTextNode(textContent);
     newElement.appendChild(textNode);
@@ -39,9 +47,24 @@ function generateElements(elementType, textContent, className) {
         newElement.classList.add(className);
     }
 
+    newElement.id = a;
+
     return newElement;
 }
 
+function generateStyles(){
+    const style = document.createElement('style');
+    style.innerHTML = `
+    .copy-btn {
+        background-color: var(--light-blue-100);
+    }
+    .copy-btn:hover {
+        cursor: pointer; 
+        background-color: var(--light-blue-400);
+    }
+    `
+    document.head.appendChild(style);
+}
 function encrypt(text) {
     let encryptedText = text;
     encryptedText = encryptedText.replace(/e/g, "enter");
@@ -72,6 +95,8 @@ function createParagraph(textContent, className) {
     if (className) {
         newElement.classList.add(className);
     }
+    
+    newElement.id = "result-p";
 
     return newElement;
 }
@@ -83,17 +108,25 @@ function encryptText(){
         alert("Please enter a text to encrypt");
         return;
     }
-    if(resultP!=null && copyBtn!=null){
-        resultP.remove();
-        copyBtn.remove();
+    if(resultPId!=null && copyBtnId!=null){
+        resultPId.remove();
+        copyBtnId.remove();
     }
     const paragraph = createParagraph(encrypt(inputText), 'result-p');
     const targetElement = document.getElementsByClassName('second')[0]; // Specify the element index
     targetElement.appendChild(paragraph);
     document.getElementsByClassName('second-central')[0].style.display = "none"; // Correctly access the style property
-    let newButton = generateElements("button", "Copy", "copy-btn");
+    let newButton = generateElements("button", "Copy", "copy-btn", "copy-btn");
     targetElement.appendChild(newButton);
+    Swal.fire({
+        title: 'ENCRYPTED!',
+        text: 'You can proceed to copy the encrypted text!',
+        icon: 'success',
+        confirmButtonText: 'Yeah!'
+      })
     setAfterBTN();
+    generateStyles();
+
 };
 
 function decryptText(){
@@ -102,21 +135,33 @@ function decryptText(){
         alert("Please enter a text to decrypt");
         return;
     }
-    if(resultP!=null && copyBtn!=null){
-        resultP.remove();
-        copyBtn.remove();
+    if(resultPId!=null && copyBtnId!=null){
+        resultPId.remove();
+        copyBtnId.remove();
     }
     const paragraph = createParagraph(decrypt(inputText), 'result-p');
     const targetElement = document.getElementsByClassName('second')[0]; // Specify the element index
     targetElement.appendChild(paragraph);
     document.getElementsByClassName('second-central')[0].style.display = "none"; // Correctly access the style property
-    let newButton = generateElements("button", "Copy", "copy-btn");
+    let newButton = generateElements("button", "Copy", "copy-btn", "copy-btn");
     targetElement.appendChild(newButton);
+    document.getElementById('btn-decode').textContent = "Decrypt!";
+    Swal.fire({
+        title: 'DECRYPTED!',
+        text: 'You can proceed to copy the decrypted text!',
+        icon: 'success',
+        confirmButtonText: 'Yeah!'
+      })
     setAfterBTN();
+    generateStyles();
 };
 
-copyBtn.addEventListener('click', function() {
-    const text = resultP.value;
+function copyText(){
+    let text = document.getElementById('result-p').textContent;
     navigator.clipboard.writeText(text);
-});
+
+    copyBtnId.textContent = "Copied!";
+}
+
+
 
